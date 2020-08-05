7ee97a4f689f6c6ba5931be8928584457bef5942
David Tran <david.hoa.tran@gmail.com>
2020-08-05 17:04:54 -0400

    Test react build.

---
+++ precache-manifest.5d62560f929150d6989df82a7f92462c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207b168367d5926e1b9f754a681a8e87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d50f5abc618a8b45c98",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70b3902f5081023862e1",
+    "url": "/static/js/2.c5ff3692.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c5ff3692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d50f5abc618a8b45c98",
+    "url": "/static/js/main.3a1aa307.chunk.js"
+  },
+  {
+    "revision": "a4b53f6516c4bf9a1053",
+    "url": "/static/js/runtime-main.53d22f30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
